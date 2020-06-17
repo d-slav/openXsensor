30d18183dff81d45e734810656a052b6e3d672e3
Dušan Slavětínský <slavetinsky@aev.cz>
2020-06-17 14:31:34 +0200

    Slozka s konfigurací

---
+++ openXsensor/examples/dslav/EagleTreeHub/oXs_config_advanced.h
@@ -0,0 +1,20 @@
+#ifndef OXS_CONFIG_ADVANCED_h
+#define OXS_CONFIG_ADVANCED_h
+
+#define PIN_SERIALTX 2
+
+#define USE_INTERNAL_REFERENCE
+
+#define PIN_VOLTAGE  1 , 8 , 8 , 8 , 8 , 8
+#define RESISTOR_TO_GROUND 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
+#define RESISTOR_TO_VOLTAGE 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
+#define OFFSET_VOLTAGE 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
+#define SCALE_VOLTAGE 1.0 , 1.0 , 1.0 , 1.0 , 1.0 , 1.0
+
+#define PIN_CURRENTSENSOR 0
+#define MVOLT_AT_ZERO_AMP 0.0
+#define MVOLT_PER_AMP     100.0
+#define RESISTOR_TO_GROUND_FOR_CURRENT 0.0
+#define RESISTOR_TO_CURRENT_SENSOR     0.0
+
+#endif

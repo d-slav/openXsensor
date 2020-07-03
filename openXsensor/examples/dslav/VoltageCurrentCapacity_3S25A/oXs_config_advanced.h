c793b5ee0d39ce739b06fad1016550f218fcfc30
Dušan Slavětínský <slavetinsky@aev.cz>
2020-07-03 11:20:36 +0200

    Add EasyStar

---
+++ openXsensor/examples/dslav/VoltageCurrentCapacity_3S25A/oXs_config_advanced.h
@@ -0,0 +1,20 @@
+#ifndef OXS_CONFIG_ADVANCED_h
+#define OXS_CONFIG_ADVANCED_h
+
+#define PIN_SERIALTX 4
+
+#define REFERENCE_VOLTAGE 3300
+
+#define PIN_VOLTAGE  0 , 8 , 8 , 8 , 8 , 8
+#define RESISTOR_TO_GROUND 5.1 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
+#define RESISTOR_TO_VOLTAGE 68.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
+#define OFFSET_VOLTAGE 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
+#define SCALE_VOLTAGE 1.0 , 1.0 , 1.0 , 1.0 , 1.0 , 1.0
+
+#define PIN_CURRENTSENSOR 1
+#define MVOLT_AT_ZERO_AMP 0.0
+#define MVOLT_PER_AMP     0.05
+#define RESISTOR_TO_GROUND_FOR_CURRENT 0.0
+#define RESISTOR_TO_CURRENT_SENSOR     0.0
+
+#endif
